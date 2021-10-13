006e8fcb9dc1ef44eb77239143fbfbeac7c3d9b9
fedeledesma <mithlond_lnr@live.com.ar>
2021-10-13 11:21:00 -0300

    otra vez

--- js/app.js
+++ js/app.js
@@ -1,36 +1,48 @@
 $(document).ready(function() {
 
     $("#calc").click(function(){
+
         let valor1 = $("#valor1").val() 
         valor1 = parseFloat(valor1)
+
         let valor2 = $("#valor2").val()
         valor2 = parseFloat(valor2)
+
         let valor3 = $("#valor3").val()
-        let resultado
-        if(valor3 == "sum"){
+
+        let resultado = 0
+
+
+
+        if(valor3 == "sum"){   
         resultado = valor1 + valor2;
-        console.log(resultado)
+        $("#resultado").text(resultado)
         }
 
     else if(valor3 == "rest"){
         resultado = valor1 - valor2;
-        console.log(resultado)
+        $("#resultado").text(resultado)
     }
 
     else if(valor3 == "div"){
         resultado = valor1 / valor2;
-        console.log(resultado)
+        $("#resultado").text(resultado)
     }
 
     else if(valor3 == "mult"){
         resultado = valor1 * valor2;
-        console.log(resultado)
+        $("#resultado").text(resultado)
     }
 
+    else if(valor3 == "pot"){
+        resultado = valor1 ** valor2;
+        $("#resultado").text(resultado)
+    }    
 
-        
+    else if(valor3 == "raiz"){
+        resultado = Math.sqrt(valor1)
+        $("#resultado").text(resultado)
+    }    
     })
-    
-
 
 });
